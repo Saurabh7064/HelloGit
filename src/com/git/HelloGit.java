77e990bce93a5381690fe3887949043e3b2c0c3f
surbhi <surbhi@gmail.com>
2016-01-11 14:48:34 +0530

    1st commit

---
+++ src/com/git/HelloGit.java
@@ -0,0 +1,10 @@
+package com.git;
+
+public class HelloGit {
+
+	
+	public HelloGit(String message){
+		System.out.println(message);
+	}
+	
+}
---
+++ src/com/main/TestGit.java
@@ -0,0 +1,12 @@
+package com.main;
+
+import com.git.HelloGit;
+
+public class TestGit {
+public static void main(String[] args) {
+	HelloGit h = new HelloGit("GIT");
+	
+}
+
+	
+}
